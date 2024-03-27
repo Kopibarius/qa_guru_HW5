811044b446da8742c714b26c9a763a655ea23dc4
Egor Melnichyk <egor.mel8@gmail.com>
2024-03-28 00:39:11 +0300

    First HW5 complite

--- src/test/java/RedirectSolutionEnterprize.java
+++ src/test/java/RedirectSolutionEnterprize.java
@@ -1,3 +1,20 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class RedirectSolutionEnterprize {
-    
+
+    @Test
+    void checkSolutionEnterprize() {
+        open("https://github.com/");
+        $(byText("Solutions")).hover();
+        $(By.linkText("Enterprise")).click();
+        $("#hero-section-brand-heading").shouldHave(
+                text("The AI-powered"),
+                text("developer platform."));
+        sleep(5000);
+    }
 }
